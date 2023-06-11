accd18486c3296248579f84f0d021b0a31b19a3e
Serref <Reef163@gmail.com>
2023-06-11 14:42:07 -0400

    Updated logo

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -11,7 +11,7 @@ const Header = () => {
           <LinkContainer to="/">
             <Navbar.Brand>
               <img src={logo} alt="ProShop Logo" />
-              ProShop
+              Ecommern
             </Navbar.Brand>
           </LinkContainer>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
