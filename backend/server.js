91b79911684c6ff0ef196cba9e99b45c9b7f2190
Serref <Reef163@gmail.com>
2023-06-13 23:54:29 -0400

    Backend authentication - created an auth middleware and implemented the logic for the user controller.

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,30 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 dotenv.config();
 import connectDB from "./config/db.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 const port = process.env.PORT || 5001;
 
 connectDB();
 
 const app = express();
 
+// Body parser middlware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Cookie parser middleware
+app.use(cookieParser());
+
 app.get("/", (req, res) => {
   res.status(200).send("API is running...");
 });
 
 app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
